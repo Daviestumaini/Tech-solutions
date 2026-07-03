0b0bd0c86e061687d4bfa525b7f85e97228dc5a5
Davies M <daviestumaini@gmail.com>
2026-07-03 13:50:23 +0300

    Update API_BASE URL for production environment

--- Js/config.js
+++ Js/config.js
@@ -2,4 +2,4 @@
 const API_BASE =
     location.hostname === "localhost"
         ? "http://localhost:5000/api"
-        : "https://gadget-finds.onrender.com";
+        : "https://tech-solutions-ap23.onrender.com";
