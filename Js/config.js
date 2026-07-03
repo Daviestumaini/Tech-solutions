2b6c760bdda7b6e6ddd45dcecf3c8f7e1b4da68f
Davies M <daviestumaini@gmail.com>
2026-07-03 15:10:08 +0300

    Update API_BASE URL to include '/api' path

--- Js/config.js
+++ Js/config.js
@@ -2,4 +2,4 @@
 const API_BASE =
     location.hostname === "localhost"
         ? "http://localhost:5000/api"
-        : "https://tech-solutions-ap23.onrender.com";
+        : "https://tech-solutions-ap23.onrender.com/api";
