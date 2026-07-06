ac4dc193025dab282fd4c3b8727b3c515521f25f
Davies M <daviestumaini@gmail.com>
2026-07-06 13:01:47 +0300

    Refactor API_BASE and update SUPABASE_ANON_KEY

--- Js/config.js
+++ Js/config.js
@@ -1,10 +1,10 @@
 const API_BASE =
-location.hostname==="localhost"
-? "http://localhost:5000/api"
-: "https://tech-solutions-k8ci.onrender.com/api";
+location.hostname === "localhost"
+    ? "http://localhost:5000/api"
+    : "https://tech-solutions-k8ci.onrender.com/api";
 
 const SUPABASE_URL =
 "https://haxftrcqcjovtembjubk.supabase.co";
 
 const SUPABASE_ANON_KEY =
-"eyJhbGciOi...";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhheGZ0cmNxY2pvdnRlbWJqdWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNTc2NzYsImV4cCI6MjA5ODYzMzY3Nn0.rDpiegF-anbolexqvUglfsMXn89rXimupGIFe4t9R8Y";
